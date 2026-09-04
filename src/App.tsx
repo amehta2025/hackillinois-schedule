1f27e30b8b866ae9fccae762f89ad1a03bbb6e56
Anish Mehta <amehta2025@gmail.com>
2026-09-04 12:33:38 -0500

    Add event types and render schedule events

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import {useEffect, useState} from 'react';  //useState: store events, useEffect: fetch events
+import type {Event} from './types';  
 
-function App() {  //this is a react component.
-  const [events, setEvents] = useState([]);
+
+function App() {  //react component.
+  const [events, setEvents] = useState<Event[]>([]);  //starts empty, but will eventually be filled with events from the HackIllinois API.
 
   useEffect(() => {
     fetch("https://adonix.hackillinois.org/event/")  //calls GET /event/ on the HackIllinois API, which returns a JSON object with an array of events.
       .then((response) => response.json()) //When the server responds, take that response and parse its body as JSON
       .then((data) => {
         console.log(data);
-        setEvents(data.events); //take the events array HackIllinois gave us and make that our component's events state.
+        setEvents(data.events); 
       });
   }, []);
 
@@ -16,6 +18,12 @@ function App() {  //this is a react component.
     <main>
       <h1> HackIllinois Schedule </h1>
       <p> Events loaded: {events.length} </p>
+      {events.map((event) => (
+        <div key={event.eventId}>
+          <h2>{event.name}</h2>
+          <p>{event.description}</p>
+        </div>
+      ))}
     </main>
   );
 }
